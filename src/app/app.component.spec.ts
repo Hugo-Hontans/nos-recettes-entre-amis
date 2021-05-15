d86cf9a392fb0b202565c6654464abd024d52d1e
Hugo-Hontans <hugohontans@gmail.com>
2021-05-15 10:57:27 +0200

    start custom button ui

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'nos-recettes-entre-amis'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('nos-recettes-entre-amis');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nos-recettes-entre-amis app is running!');
-  });
 });
